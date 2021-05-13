8c1ba8134b736181a1faf39214a403981b1239b0
Yuuki0205 <yuuki.2.5@icloud.com>
2021-05-13 16:07:09 +0900

    モデルの単体テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          validates :nickname, presence: true
-         validates :first_name, presence: true
-         validates :last_name, presence: true
-         validates :first_name_kana, presence: true
-         validates :last_name_kana, presence: true
+         validates :first_name, presence: true, format: {with:  /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+         validates :last_name, presence: true, format: {with:  /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+         validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
+         validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
          validates :birth_date, presence: true
+         validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message:"は半角英数混合でお願いします" }
 
          has_many :items
          has_many :purchase_histories
---
+++ spec/factories/users.rb
@@ -0,0 +1,75 @@
+FactoryBot.define do
+  factory :user do
+    nickname {"name"}
+    email {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 6)
+    password {password}
+    password_confirmation {password}
+    first_name {"太郎"}
+    last_name {"山田"}
+    first_name_kana {"タロウ"}
+    last_name_kana {"ヤマダ"}
+    birth_date {"2000-02-05"}
+
+  end
+end
+
+
+
+
+# it 'passwordが空だと登録できない' do
+#   user = FactoryBot.build(:user)
+#   user.password = ""
+#   user.valid?
+#   expect(user.errors.full_messages).to include("Password can't be blank")
+# end
+
+
+
+# it 'passwordが空だと登録できない' do
+#   user = User.new(
+#     nickname:"test",
+#     email: "test@example.com",
+#     password: "",
+#     password_confirmation: "test11",
+#     first_name: "太郎",
+#     last_name: "山田",
+#     first_name_kana: "タロウ",
+#     last_name_kana: "ヤマダ",
+#     birth_day: "2000-08-12"
+#   )
+#   user.valid?
+#   expect(user.errors.full_messages).to include("Password can't be blank")
+# end
+
+# it 'emailが空だと登録できない' do
+#   user = User.new(
+#     nickname:"test",
+#     email: "",
+#     password: "test11",
+#     password_confirmation: "test11",
+#     first_name: "太郎",
+#     last_name: "山田",
+#     first_name_kana: "タロウ",
+#     last_name_kana: "ヤマダ",
+#     birth_day: "2000-08-12"
+#   )
+#   user.valid?
+#   expect(user.errors.full_messages).to include("Password can't be blank")
+# end
+
+# it 'nicknameが空だと登録できない' do
+#   user = User.new(
+#     nickname:"",
+#     email: "test@example.com",
+#     password: "test11",
+#     password_confirmation: "test11",
+#     first_name: "太郎",
+#     last_name: "山田",
+#     first_name_kana: "タロウ",
+#     last_name_kana: "ヤマダ",
+#     birth_day: "2000-08-12"
+#   )
+#   user.valid?
+#   expect(user.errors.full_messages).to include("Password can't be blank")
+# end
