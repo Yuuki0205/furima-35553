bb07488729638cae7283cdd8fdd45ca498759359
Yuuki0205 <yuuki.2.5@icloud.com>
2021-05-14 17:13:09 +0900

    ActiveHashとActive Storageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :product_category
+  belongs_to_active_hash :product_condition
+  belongs_to_active_hash :shipping_charges
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :days_ship
+ 
 end
