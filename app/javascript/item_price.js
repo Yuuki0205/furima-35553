1498736b25db479e1e3784394f88ae9cfdd1bd65
Yuuki0205 <yuuki.2.5@icloud.com>
2021-05-16 15:02:05 +0900

    手数料や利益の計算結果の実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', () => {
+  
+  const priceInput = document.getElementById("item-price");
+  
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    
+    const feeInput = document.getElementById("add-tax-price");
+    console.log(feeInput);
+    feeInput.innerHTML = (Math.floor(inputValue * 0.1));
+    const profitInput = document.getElementById("profit");
+
+    profitInput.innerHTML = (Math.floor(inputValue * 0.9));
+  })
+});
