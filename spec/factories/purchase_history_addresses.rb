6cbb372f7c4f74d1ec09ac1b2118b6b24b0fe69a
Yuuki0205 <yuuki.2.5@icloud.com>
2021-05-21 11:14:04 +0900

    PAY.jpを除いたテストコードの実装

--- spec/factories/purchase_history_addresses.rb
+++ spec/factories/purchase_history_addresses.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :purchase_history_address do
-    
+    postal_code { '123-4567' }
+    prefecture_id { 2 }
+    city { '東京都' }  
+    address {' 住所 '}
+    phone_number { '09012345678' }
+    building_name { '' }
   end
 end
